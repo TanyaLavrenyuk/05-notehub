9fe5d52bf261089dd3832fc277e9d07f389ecb01
Tanya Lavrenyuk <tlawrenyuk43@gmail.com>
2026-07-03 22:11:21 +0300

    fix: remove broken global css imports for vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "modern-normalize";
 import App from "./components/App/App";
 
 const queryClient = new QueryClient();
